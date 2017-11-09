6a8b8b7e55061afe9c26b3e604aa4677f18c2372
Chunsang Jeong <chunsangjeong@gmail.com>
2017-11-09 16:38:15 +0900

    add fb number as input
    Signed-off-by: Chunsang Jeong <chunsangjeong@gmail.com>

--- mx6/utils/position/position.c
+++ mx6/utils/position/position.c
@@ -13,19 +13,59 @@
 #include <linux/mxcfb.h>
 #include <linux/ipu.h>
 
+#define NUM_FB 4
+
+static int print_fbinfo (struct fb_var_screeninfo fb) {
+    printf("\nFB information \n");
+    printf("xres = %d\n",  fb.xres);
+    printf("xres_virtual = %d\n",  fb.xres_virtual);
+    printf("yres = %d\n",  fb.yres);
+    printf("yres_virtual = %d\n",  fb.yres_virtual);
+    printf("bits_per_pixel = %d\n",  fb.bits_per_pixel);
+    printf("pixclock = %d\n",  fb.pixclock);
+    printf("height = %d\n",  fb.height);
+    printf("width = %d\n",  fb.width);
+    printf("Pixel format : RGBX_%d%d%d%d\n",fb.red.length,
+                                                 fb.green.length,
+                                                 fb.blue.length,
+                                                 fb.transp.length);
+    printf(" Begin of bitfields(Byte ordering):-\n");
+    printf("  Red    : %d\n",fb.red.offset);
+    printf("  Blue   : %d\n",fb.blue.offset);
+    printf("  Green  : %d\n",fb.green.offset);
+    printf("  Transp : %d\n\n",fb.transp.offset);
+
+    return 0;
+}
 
 int main (int argc, char *argv[])
 {
     struct fb_var_screeninfo fb0_var;
     struct fb_fix_screeninfo fb0_fix;
     int fd_fb0;
+    char fb_name[]="/dev/fb";
 
     struct mxcfb_pos pos;
     int width, height;
 
+    if (argc < 6) //fb, xoff, yoff, xres, yres
+    {
+        printf(" Put fb,x,y,w,h argument\n");
+        exit(-1);
+    }
+
+    /*check framebuffer number*/
+    if (atoi(argv[1]) < 0 || atoi(argv[1]) > NUM_FB) {
+        printf(" Put fb,x,y,w,h argument\n");
+        exit(-1);
+    }
+
+    strcat(fb_name, argv[1]);
+
+    printf("Opening %s\n",fb_name);
     // Open Framebuffer and gets its address
-    if ((fd_fb0 = open("/dev/fb1", O_RDWR, 0)) < 0) {
-        printf("Unable to open /dev/fb1\n");
+    if ((fd_fb0 = open(fb_name, O_RDWR, 0)) < 0) {
+        printf("Unable to open %s\n",fb_name);
         goto done;
     }
 
@@ -41,36 +81,13 @@ int main (int argc, char *argv[])
         goto done;
     }
 
-    printf("\nFB information \n");
-    printf("xres = %d\n",  fb0_var.xres);
-    printf("xres_virtual = %d\n",  fb0_var.xres_virtual);
-    printf("yres = %d\n",  fb0_var.yres);
-    printf("yres_virtual = %d\n",  fb0_var.yres_virtual);
-    printf("bits_per_pixel = %d\n",  fb0_var.bits_per_pixel);
-    printf("pixclock = %d\n",  fb0_var.pixclock);
-    printf("height = %d\n",  fb0_var.height);
-    printf("width = %d\n",  fb0_var.width);
-    printf("Pixel format : RGBX_%d%d%d%d\n",fb0_var.red.length,
-                                                 fb0_var.green.length,
-                                                 fb0_var.blue.length,
-                                                 fb0_var.transp.length);
-    printf(" Begin of bitfields(Byte ordering):-\n");
-    printf("  Red    : %d\n",fb0_var.red.offset);
-    printf("  Blue   : %d\n",fb0_var.blue.offset);
-    printf("  Green  : %d\n",fb0_var.green.offset);
-    printf("  Transp : %d\n",fb0_var.transp.offset);
+    print_fbinfo(fb0_var);
 
-    if (argc < 5)
-    {
-        printf(" Put x,y,w,h argument\n");
-        exit(-1);
-    }
-    
     // frame position, width and height
-    pos.x = atoi(argv[1]);
-    pos.y = atoi(argv[2]);
-    width = atoi(argv[3]);
-    height = atoi(argv[4]);
+    pos.x = atoi(argv[2]);
+    pos.y = atoi(argv[3]);
+    width = atoi(argv[4]);
+    height = atoi(argv[5]);
     
     fb0_var.xres = fb0_var.xres_virtual = width ;
     fb0_var.yres = height ;
